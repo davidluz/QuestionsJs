1261e299153225c18f725d8a68e1ca74df2ae526
davidluz <davidmelo8@hotmail.com>
2015-10-23 15:44:53 -0200

    v 1.0

--- js/elearningComponents.js
+++ js/elearningComponents.js
@@ -1,29 +1,15 @@
-// JavaScript Document
-var questions = [];
+var questions = []; 
+var ids = 0;
+
+// Função que cria questão
 
 
+// Definicão de uma questão 
 
 
-// Atributos
-/* 
-at1 = Enunciado.
-at2 = Alternativa 1
-at3 = Alternativa 2
-at4 = Alternativa 3
-at5 = Alternativa 4
-at6 = Alternativa 5
-at7 = Feedback geral acerto
-at8 = Feedback geral erro
-at9 = Feedback 1
-at10 = Feedback 2
-at11 = Feedback 3
-at12 = Feedback 4
-at13 = Feedback 5  
- */
-
-
-function Questao(at1,at2,at3, at4, at5,at6,at7,at8,at9,at10,at11,at12,at13){
-
+function Questao(id,at0,at1,at2,at3, at4, at5,at6,at7,at8,at9,at10,at11,at12,at13){
+    this.id =  id;
+	this.correctAlternative = at0;
 	this.enunciation =  at1;
 	this.alternative1 = at2;
 	this.alternative2 = at3;
@@ -39,13 +25,70 @@ function Questao(at1,at2,at3, at4, at5,at6,at7,at8,at9,at10,at11,at12,at13){
 	this.feedback5 = at13;
 }
 
-$("#elearningComponents-container").hide();	
+
+
+// Renderiza questão na tela
+function questionsRender(){
+	
+	for (var i=0; i<questions.length; i++){
+		
+		$("#elearningComponents-container").append('<form class="questions" id="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].enunciation);
+		$("#elearningComponents-container").append("<br>");
+		$("#elearningComponents-container").append("<br>");
+		
+		$("#elearningComponents-container").append('<input type="radio" value="1" name="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].alternative1);
+		$("#elearningComponents-container").append("<br>");
+		
+		$("#elearningComponents-container").append('<input type="radio" value="2" name="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].alternative2);
+		$("#elearningComponents-container").append("<br>");
+		
+		$("#elearningComponents-container").append('<input type="radio"  value="3" name="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].alternative3);
+		$("#elearningComponents-container").append("<br>");
+		
+		$("#elearningComponents-container").append('<input type="radio" value="4" name="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].alternative4);
+		$("#elearningComponents-container").append("<br>");
+		
+		$("#elearningComponents-container").append('<input type="radio"  value="5" name="questionNumber'+i +'">');
+		$("#elearningComponents-container").append(questions[i].alternative5);		
+		$("#elearningComponents-container").append("</form>");
+		$("#elearningComponents-container").append("<br>");
+		$("#elearningComponents-container").append("<br>");
+		$("#elearningComponents-container").append('<button onclick="verificaResposta('+i+')">Verficar</button>');
+		
+	}
+	
+}
 
  
 
 
 $(document).ready(function(e) {
-   
-
+	
+var questao1 = new Questao(1,4,"Qual é o nome do meu cachorro ?","paulo","andre","pedro","teste","teste");
+questions.push(questao1);
+questionsRender();
 
 });
+
+
+// Verifica se resposta está correta
+function verificaResposta(i){
+	
+	var resposta = $("input[name='questionNumber0']:checked").val();
+	
+	if(resposta==questions[i].correctAlternative){
+		
+		//Colocar aqui a ação para a resposta correta
+		console.log('respostaCerta');
+		
+		}
+	
+}
+
+
+
